xabbuh/symfony

deprecate the TransportFactoryTestCase

src/Symfony/Component/Mailer/Bridge/Brevo/Tests/Transport/BrevoTransportFactoryTest.php
@@ -16,12 +16,15 @@ use Symfony\Component\HttpClient\MockHttpClient;
 use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoApiTransport;
 use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoSmtpTransport;
 use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoTransportFactory;
-use Symfony\Component\Mailer\Test\TransportFactoryTestCase;
+use Symfony\Component\Mailer\Test\AbstractTransportFactoryTestCase;
+use Symfony\Component\Mailer\Test\IncompleteDsnTestTrait;
 use Symfony\Component\Mailer\Transport\Dsn;
 use Symfony\Component\Mailer\Transport\TransportFactoryInterface;
 
-class BrevoTransportFactoryTest extends TransportFactoryTestCase
+class BrevoTransportFactoryTest extends AbstractTransportFactoryTestCase
 {
+    use IncompleteDsnTestTrait;
+
     public function getFactory(): TransportFactoryInterface
     {
         return new BrevoTransportFactory(null, new MockHttpClient(), new NullLogger());

symfony/brevo-notifier

Tests/BrevoTransportFactoryTest.php
@@ -12,10 +12,15 @@
 namespace Symfony\Component\Notifier\Bridge\Brevo\Tests;
 
 use Symfony\Component\Notifier\Bridge\Brevo\BrevoTransportFactory;
-use Symfony\Component\Notifier\Test\TransportFactoryTestCase;
+use Symfony\Component\Notifier\Test\AbstractTransportFactoryTestCase;
+use Symfony\Component\Notifier\Test\IncompleteDsnTestTrait;
+use Symfony\Component\Notifier\Test\MissingRequiredOptionTestTrait;
 
-final class BrevoTransportFactoryTest extends TransportFactoryTestCase
+final class BrevoTransportFactoryTest extends AbstractTransportFactoryTestCase
 {
+    use IncompleteDsnTestTrait;
+    use MissingRequiredOptionTestTrait;
+
     public function createFactory(): BrevoTransportFactory
     {
         return new BrevoTransportFactory();